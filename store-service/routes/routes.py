92b95b41e1454da9c040a75f911037e1b2a0b8ff
gubanovpm <petrgubanov37@gmail.com>
2024-11-22 23:24:03 +0300

    added cart and library page

--- store-service/routes/routes.py
+++ store-service/routes/routes.py
@@ -23,4 +23,13 @@ def open_game_page():
 
 @routes_blueprint.route('/auth')
 def open_auth_page():
-    return render_template('Authorize.html')
+    return render_template('Authorisation.html')
+
+@routes_blueprint.route('/cart')
+def open_cart_page():
+    return render_template('Cart.html')
+
+@routes_blueprint.route('/library')
+def open_library_page():
+    return render_template('Library.html')
+
