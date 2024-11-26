ed24922f052c51726775fac0727071c271deef6a
gubanovpm <petrgubanov37@gmail.com>
2024-11-27 00:27:38 +0300

    now pictures also gets from database

---
+++ store-service/utility/DescriptionRedactor.py
@@ -0,0 +1,9 @@
+      
+import bleach
+
+ALLOWED_TAGS = ['p', 'b', 'i', 'u', 'strike', 'a', 'img', 'h1', 'h2', 'h3', 'ul', 'ol', 'li', 'blockquote']
+ALLOWED_ATTRIBUTES = {'a': ['href', 'title'], 'img': ['src', 'alt']}
+
+def sanitize_html(html):
+	cleaned_html = bleach.clean(html, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES, strip=True)
+	return cleaned_html
--- store-service/utility/Game.py
+++ store-service/utility/Game.py
@@ -1,17 +1,38 @@
 class Game:
 	name: str
 	price: float
-	cover: str
 	description: str
-	screenshots: list[str]
-	tags: list[str]
-	stuido: str
 	game_id: int
+	
+	cover: str
+	screenshots: list[str]
+	icon: str
+
+	tags: list[str]
+	studio: str
 
 	def __init__(self, data:dict):
+		print(data)
 		self.game_id= data['id']
 		self.name   = data['name']
 		self.price  = data['price']
-		self.stuido = data['studio_id']
 		self.description = data['description']
-		self.cover = 'dota.jpg'
+
+		if 'studio_name' in data:
+			self.studio = data['studio_name']
+		if 'tags' in data:
+			self.tags   = data['tags'].split(',') if data['tags'] else []
+		if 'pictures' in data:
+			pic_data = data['pictures'].split(',') if data['pictures'] else []
+			self.screenshots = []
+			for pic in pic_data:
+				info = pic.split(':')
+				match info[1]:
+					case 'cover':
+						self.cover = info[0]
+					case 'icon':
+						self.icon = info[0]
+					case 'screenshot':
+						self.screenshots.append(info[0])
+
+		print(vars(self))
